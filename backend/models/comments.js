f6342209f181b61e9ace426b13f15c56edc3d839
mayorr-star <immaboat11@gmail.com>
2024-11-12 21:33:59 +0000

    Add query to delete comment and create new comments

--- backend/models/comments.js
+++ backend/models/comments.js
@@ -10,4 +10,23 @@ const deleteCommentByPostId = async (postId) => {
   });
 };
 
-module.exports = { deleteCommentByPostId };
+const deleteComment = async (id) => {
+  await prisma.comment.delete({
+    where: {
+      id: id,
+    },
+  });
+};
+
+const createComment = async (content, authorId, postId) => {
+  const comment = await prisma.comment.create({
+    data: {
+      content: content,
+      authorId: authorId,
+      postId: postId,
+    },
+  });
+  return comment;
+};
+
+module.exports = { deleteCommentByPostId, deleteComment, createComment };
