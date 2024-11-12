1ab408eb7d5b44ae800aa3950de05fd120a834c0
mayorr-star <immaboat11@gmail.com>
2024-11-12 21:31:56 +0000

    Add parent routes for comments

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const postRouter = require("./routes/postRouter");
 const authRouter = require("./routes/authRouter");
+const commentRouter = require('./routes/commentRouter');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/posts/:postId/comments", commentRouter);
 app.use("/posts", postRouter);
 app.use("/user", authRouter);
 
