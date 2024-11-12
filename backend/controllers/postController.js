d4fe89e4666e5fc9346a613a56198a785da23166
mayorr-star <immaboat11@gmail.com>
2024-11-12 12:36:13 +0000

    Add controllers to update and delete posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,21 +1,50 @@
-const model = require("../models/posts");
+const postModel = require("../models/posts");
+const commentModel = require('../models/comments');
+const { validationResult } = require("express-validator");
+const { validateNewPost } = require("../utilis/validators/validators");
 
 const getAllPosts = async (req, res) => {
-  const { published } = req.query;
-  if (published) {
-    const posts = await model.getAllPosts();
-    res.json(posts);
-  }
+  const posts = await postModel.getAllPosts();
+  res.json({ posts });
 };
 
 const getPostById = async (req, res) => {
   const { postId } = req.params;
-  const post = await model.getPostById(postId);
-  res.json(post);
+  const post = await postModel.getPostById(postId);
+  res.json({ post });
 };
 
-const createPost = async (req, res) => {
-  res.json('post created...')
-}
+const createPost = [
+  validateNewPost,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ error: errors.array() });
+    const { title, content, publish } = req.body;
+    const { authorId } = req.params;
+    const post = await postModel.createPost(title, content, authorId, publish);
+    res.status(201).json({ message: "Post created...", post });
+  },
+];
 
-module.exports = { getAllPosts, getPostById, createPost };
+const updatePost = [
+  validateNewPost,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ error: errors.array() });
+    const { title, content, publish } = req.body;
+    const { postId } = req.params;
+    const updatedPost = await postModel.updatePost(title, content, postId, publish);
+    res.json({ message: "Post updated...", updatedPost });
+  },
+];
+
+const deletePost = async (req, res) => {
+  const { postId } = req.params;
+  await commentModel.deleteCommentByPostId(postId);
+  await postModel.deletePost(postId);
+  res.json({message: 'Post deleted...'})
+};
+
+module.exports = { getAllPosts, getPostById, createPost, updatePost, deletePost };
