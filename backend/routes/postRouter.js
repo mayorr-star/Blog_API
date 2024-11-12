f60cdc5da94fdd74d8354405f9234a3d8fb1bfd8
mayorr-star <immaboat11@gmail.com>
2024-11-12 12:40:22 +0000

    Add routes for updating and deleting

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,10 +1,12 @@
-const { Router } = require('express');
-const postController = require('../controllers/postController');
-const {verifyToken} = require('../controllers/authController')
+const { Router } = require("express");
+const postController = require("../controllers/postController");
+const { verifyToken } = require("../controllers/authController");
 const router = Router();
 
-router.get('/', postController.getAllPosts);
-router.get('/:postId', postController.getPostById);
-router.post('/', verifyToken, postController.createPost)
+router.get("/", postController.getAllPosts);
+router.get("/:postId", postController.getPostById);
+router.post("/:authorId", verifyToken, postController.createPost);
+router.put("/:postId", verifyToken, postController.updatePost);
+router.delete("/:postId", verifyToken, postController.deletePost);
 
 module.exports = router;
