01a8dea9cb894e1d30e026c0fa404ee91b2db1ec
mayorr-star <immaboat11@gmail.com>
2024-11-13 11:35:26 +0000

    Create mock users with posts and comments

---
+++ backend/models/mock.js
@@ -0,0 +1,51 @@
+const { PrismaClient } = require("@prisma/client");
+const { faker } = require("@faker-js/faker");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.comment.deleteMany();
+  await prisma.post.deleteMany();
+  await prisma.user.deleteMany();
+  console.log("Populating DB...");
+  for (let i = 0; i < 10; i++) {
+    const user = await prisma.user.create({
+      data: {
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      },
+    });
+    console.log(user) 
+
+    // Create posts
+    const post = await prisma.post.create({
+      data: {
+        title: faker.lorem.lines({min: 1, max: 3}),
+        content: faker.lorem.paragraphs({min: 5, max: 10}),
+        author: { connect: { id: user.id } },
+        published: true
+      },
+    });
+
+    // Create comments
+    const comment = await prisma.comment.create({
+      data: {
+        content: faker.lorem.paragraphs({min: 5, max: 10}),
+        post: { connect: { id: post.id } },
+        author: { connect: { id: user.id } },
+      },
+    });
+  }
+  console.log("DB populated.");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
